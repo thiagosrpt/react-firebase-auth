ba1b4474c284268addf9e01d1dca44a7fdfbb2ca
Thiago Petrimperni <thiago.srpt@gmail.com>
2023-02-23 23:47:28 -0500

    chore: add files

--- src/Home.js
+++ src/Home.js
@@ -4,6 +4,7 @@ import CurrentBalance from "./CurrentBalance"
 
 import "firebase/database";
 import "firebase/auth";
+import firebase from "firebase/app";
 
 //boostrap
 import Card from 'react-bootstrap/Card';
@@ -11,6 +12,7 @@ import Card from 'react-bootstrap/Card';
 const Home = () => {
   
   const userContext = useContext(UserContext)
+  const isFirstLogin = firebase.auth().currentUser.metadata.creationTime === firebase.auth().currentUser.metadata.lastSignInTime;
 
   return (
   <>
@@ -25,6 +27,7 @@ const Home = () => {
             </Card.Body>
             <Card.Img variant="top" src="https://media.istockphoto.com/id/1280155654/photo/financial-planning-family-mother-father-and-children-with-piggy-bank-at-home.jpg?s=612x612&w=0&k=20&c=3qAf4qjUeF2e4OOT5qlSGPgmop0_8OjzndeZ29BR8zc=" />
           </Card>
+          {isFirstLogin ? <div class="msg alert alert-success">Congrats! Your account has been successfully created.</div> : ''}
       </div>
       )}
 </>
